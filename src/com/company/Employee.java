9fb5a5968cd5d2b759c9d8063304f3be4cb581ac
89081314528 <89081314528@mail.ru>
2019-09-17 13:02:28 +0300

    17/09 12.22

---
+++ src/com/company/Employee.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Employee {
+    private final String name;
+    private final String surname;
+    private final String patronymic;
+    private final int salary;
+
+
+    public Employee(String name, String surname, String patronymic, int salary) {
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+}
