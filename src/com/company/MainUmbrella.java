b53baa0e998be6205fca6c457d3b72a994ac8b73
89081314528 <89081314528@mail.ru>
2019-10-31 20:16:00 +0300

    31/10

--- src/com/company/MainUmbrella.java
+++ src/com/company/MainUmbrella.java
@@ -30,28 +30,23 @@ public class MainUmbrella {
     }
     public static void okrugleniye (List<Umbrella> umbrellas, int chislo) {
         for (int i = 0; i < umbrellas.size(); i++) {
+            System.out.println(umbrellas.get(i).getName());
             Umbrella umbrella = umbrellas.get(i);
             if(umbrella.getSalary() % chislo < 500) {
                 umbrellas.set(i,umbrella.withSalary(umbrella.getSalary() - umbrella.getSalary() % chislo));
-//                umbrella = umbrella.withSalary(umbrella.getSalary() - umbrella.getSalary() % chislo);
             } else {
                 umbrellas.set(i, umbrella.withSalary(umbrella.getSalary() + (1000 - umbrella.getSalary() % chislo)));
-//                umbrella = umbrella.withSalary(umbrella.getSalary() + (1000 - umbrella.getSalary() % chislo));
-//распечатается округленная зп, а в csv добавится изначальная
             }
-            umbrella = umbrellas.get(i);
-//            umbrellas.set(i, umbrella) = umbrella;
-//как сделать, чтобы в csv добавилась новая зп
-            System.out.println(umbrella.getSalary());
+            System.out.println(umbrellas.get(i).getSalary());
         }
     }
 
-    public static void printCsv(String filename, List<Umbrella> umbrella) throws FileNotFoundException {
+    public static void printCsv(String filename, List<Umbrella> umbrellas) throws FileNotFoundException {
         PrintStream printer = new PrintStream(new File(filename));
         printer.println("name" + ";" + "surname" + ";" + "salary");
-        for (int i = 0; i < umbrella.size(); i++){
-            Umbrella umbrellas = umbrella.get(i);
-            printer.println(umbrellas.getName() + ";" + umbrellas.getSurname() + ";" + umbrellas.getSalary());
+        for (int i = 0; i < umbrellas.size(); i++){
+            Umbrella umbrella = umbrellas.get(i);
+            printer.println(umbrella.getName() + ";" + umbrella.getSurname() + ";" + umbrella.getSalary());
         }
     }
 }
